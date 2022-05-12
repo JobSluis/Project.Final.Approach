a951eca44f3811ee5f955e8eb5dd6a2e925a2571
Bas Meddeler <bejmeddeler@gmail.com>
2022-05-12 19:43:43 +0200

    Added audio class and all the sounds

---
+++ GXPEngine/Custom/AudioPlayer.cs
@@ -0,0 +1,20 @@
+﻿namespace GXPEngine.Custom
+{
+    public static class AudioPlayer
+    {
+        public static float volume = 1;
+
+
+        public static void PlayAudio(string audio)
+        {
+            
+            new Sound(audio).Play(volume: volume);
+        }
+
+        public static SoundChannel PlayAudioSong(string audio)
+        {
+            Sound s = new Sound(audio, true, true);
+            return s.Play(volume: volume);
+        }
+    }
+}
